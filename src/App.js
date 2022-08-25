9b4059248ce47b3da83d600f98e23689a3261940
Ayax12 <jjgtoledo95@gmail.com>
2022-08-25 21:10:03 +0100

    Se sube build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import imagenRickMorty from './img/rick-morty.png'
 import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () =>{
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  }
+
+/**La interrogación de la funhción de abajo es por si la variable está rellena o
+ * es true, que es lo mismo. En caso contrario se usan los : si está vacío o es false
+ * ocurre lo que hay entre paréntesis
+ */
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title"> Rick & Morty</h1>
+
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+        ) : (
+          <>
+            <img src={imagenRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+
       </header>
     </div>
   );
